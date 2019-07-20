97a6aa3a9da1878d4ccefa8215bc83fee7535ef2
JafeelKhanZada <jafeelkhanzada@gmail.com>
2019-07-20 20:36:17 +0500

    Seller And User Class

--- project.cpp
+++ project.cpp
@@ -15,3 +15,255 @@
 #include "rapidjson/ostreamwrapper.h"
 using namespace rapidjson;
 using namespace std;
+//User Class Starting
+class User
+{
+private:
+    string firstName, lastName, userName, passWord, eMail, gender, city, state, country, phoneNumber, joinDate, accountType;
+    string id;
+
+public:
+    User()
+    {
+        firstName = "";
+        lastName = "";
+        phoneNumber = "";
+        gender = "";
+        passWord = "";
+        userName = "";
+        city = "";
+        state = "";
+        eMail = "";
+        country = "";
+        id = "";
+        joinDate = "";
+        accountType = "";
+    }
+    //Encapsulation!
+    //Setter Of Class
+    void SetFirstName(string);
+    void SetLastName(string);
+    void SetUserName(string);
+    void SetPhoneNumber(string);
+    void SetGender(string);
+    void SetPassword(string);
+    void SetCity(string);
+    void SetState(string);
+    void SetEmail(string);
+    void SetCountry(string);
+    void SetId(string);
+    void SetJoinDate(string);
+    void SetAccountType(string);
+    //Getter Of Class
+    string GetFirstName();
+    string GetLastName();
+    string GetUserName();
+    string GetPhoneNumber();
+    string GetGender();
+    string GetPassword();
+    string GetState();
+    string GetCity();
+    string GetEmail();
+    string GetCountry();
+    string GetId();
+    string GetJoinDate();
+    string GetAccountType();
+};
+
+//03205946869
+//Scoper Resolution Classes Setter Functions
+void User::SetFirstName(string name)
+{
+    firstName = name;
+};
+void User::SetLastName(string name)
+{
+    lastName = name;
+};
+void User::SetUserName(string name)
+{
+    userName = name;
+};
+void User::SetGender(string gen)
+{
+    gender = gen;
+};
+void User::SetPassword(string pass)
+{
+    passWord = pass;
+};
+void User::SetPhoneNumber(string phone)
+{
+    phoneNumber = phone;
+};
+void User::SetCity(string val)
+{
+    city = val;
+};
+void User::SetState(string val)
+{
+    state = val;
+};
+void User::SetEmail(string mail)
+{
+    eMail = mail;
+};
+void User::SetCountry(string val)
+{
+    country = val;
+};
+void User::SetId(string val)
+{
+    id = val;
+}
+void User::SetJoinDate(string val)
+{
+    joinDate = val;
+};
+void User::SetAccountType(string val)
+{
+    accountType = val;
+};
+string User::GetAccountType()
+{
+    return accountType;
+}
+//Scoper Resolution Classes Getter Functions
+string User::GetFirstName()
+{
+    return firstName;
+};
+string User::GetLastName()
+{
+    return lastName;
+};
+string User::GetUserName()
+{
+    return userName;
+};
+string User::GetPassword()
+{
+    return passWord;
+};
+string User::GetPhoneNumber()
+{
+    return phoneNumber;
+};
+string User::GetGender()
+{
+    return gender;
+};
+string User::GetCity()
+{
+    return city;
+};
+string User::GetState()
+{
+    return state;
+};
+string User::GetEmail()
+{
+    return eMail;
+};
+string User::GetCountry()
+{
+    return country;
+};
+string User::GetId()
+{
+    return id;
+};
+string User::GetJoinDate()
+{
+    return joinDate;
+};
+//User Class Ends
+
+//Seller Class Start
+class Seller : public User
+{
+private:
+    int totalAds, allowedAds, activeAds, soldGoods;
+    float revenue;
+    string subScription;
+
+public:
+    Seller()
+    {
+        totalAds = 0;
+        allowedAds = 0;
+        activeAds = 0;
+        soldGoods = 0;
+        revenue = 0.0;
+        subScription = "";
+    }
+    //Class Setter Function
+    void SetTotalAds(int);
+    void SetAllowedAds(int);
+    void SetActiveAd(int);
+    void SetSoldGood(int);
+    void SetRevenue(float);
+    void SetSubscription(string);
+    //class Getter Functions
+    int GetTotalAds();
+    int GetAllowedAds();
+    int GetActiveAd();
+    int GetSoldGood();
+    float GetRevenue();
+    string GetSubscription();
+};
+
+//Scope Revolution Setter Function
+void Seller::SetTotalAds(int val)
+{
+    totalAds = val;
+};
+void Seller::SetAllowedAds(int val)
+{
+    allowedAds = val;
+};
+void Seller::SetActiveAd(int val)
+{
+    activeAds = val;
+};
+void Seller::SetSoldGood(int val)
+{
+    soldGoods = val;
+};
+void Seller::SetRevenue(float val)
+{
+    revenue = val;
+};
+void Seller::SetSubscription(string val)
+{
+    subScription = val;
+};
+//Scope Resolution Getter Function
+int Seller::GetTotalAds()
+{
+    return totalAds;
+};
+int Seller::GetAllowedAds()
+{
+    return allowedAds;
+};
+int Seller::GetActiveAd()
+{
+    return activeAds;
+};
+int Seller::GetSoldGood()
+{
+    return soldGoods;
+};
+float Seller::GetRevenue()
+{
+    return revenue;
+};
+string Seller::GetSubscription()
+{
+    return subScription;
+};
+//Seller Class End's
+int main()
+{
+}
