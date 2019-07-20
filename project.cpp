17351aa102c1d354e2e5ef3b5da05e1d941ed7d9
JafeelKhanZada <jafeelkhanzada@gmail.com>
2019-07-20 20:29:52 +0500

    Library

---
+++ project.cpp
@@ -0,0 +1,17 @@
+// Including Header Files!
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <array>
+#include <stdlib.h>
+//Including External Library!
+#include "xlsxwriter.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/ostreamwrapper.h"
+using namespace rapidjson;
+using namespace std;
